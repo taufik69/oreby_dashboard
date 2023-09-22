413205cc33307e568fc49a7ed1fc2d276e957e9d
Taufik islam <taufik35-2915@diu.edu.bd>
2023-09-22 21:11:49 +0600

    wasywang

--- src/Component/AddProduct.jsx
+++ src/Component/AddProduct.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { EditorState } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 
 const AddProduct = () => {
-  return <div>AddProucut</div>;
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const onEditorStateChange = (newEditorState) => {
+    // Update the editorState using setEditorState
+    setEditorState(newEditorState);
+  };
+  return (
+    <div>
+      <Editor
+        editorState={editorState}
+        toolbarClassName="toolbarClassName"
+        wrapperClassName="wrapperClassName"
+        editorClassName="editorClassName"
+        onEditorStateChange={onEditorStateChange}
+      />
+    </div>
+  );
 };
 
 export default AddProduct;
