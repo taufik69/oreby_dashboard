3c02d5cbb026914a3d40ebedc7b3adc17277c3de
taufik69 <taufik35-2915@diu.edu.bd>
2023-11-13 02:23:36 +0600

    add product sucessful and showing error in dashboarde

--- src/Component/AddProduct.jsx
+++ src/Component/AddProduct.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { EditorState } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
-import { Input, Select, Tag } from "antd";
+import { Input, Select, Tag, Button } from "antd";
+import { UploadOutlined } from "@ant-design/icons";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import PrdouctUpload from "./ProductUplad/PrdouctUpload";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import axios from "axios";
 
@@ -19,6 +23,7 @@ const tagRender = (props) => {
       onClose={onClose}
       style={{
         marginRight: 3,
+        color: "purple",
       }}
     >
       {label}
@@ -36,6 +41,12 @@ const AddProduct = () => {
 
   const [editorState, setEditorState] = useState(EditorState.createEmpty());
   const [AllstoreData, setAllstoreData] = useState([]);
+  const [productName, setproductName] = useState(null);
+  const [productDescription, setproductDescription] = useState(null);
+  const [StoreName, setStoreName] = useState(null);
+  // this state work for button
+  const [size, setSize] = useState("large");
+
   const onEditorStateChange = (newEditorState) => {
     // Update the editorState using setEditorState
     setEditorState(newEditorState);
@@ -80,17 +91,78 @@ const AddProduct = () => {
     };
   }, []);
 
+  /**
+   * todo: HandleProductUpload function work for upload all details in backend
+   *
+   */
+  const HandleProductUpload = async () => {
+    try {
+      const data = await axios.post(
+        "http://localhost:3000/api/v1/product/createproduct",
+        {
+          name: productName,
+          description: productDescription,
+          store: StoreName,
+        }
+      );
+      if (data.data) {
+        toast.success(`${data.data.name} upload suessfully`, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      }
+    } catch (error) {
+      if (error.response.data.error) {
+        toast.error(`${error.response.data.error}`, {
+          position: "top-right",
+          autoClose: 6000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "colored",
+        });
+      }
+      console.log();
+    }
+  };
+
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+
+      <ToastContainer />
       <h5>product Name</h5>
 
-      <Input placeholder="Basic usage" />
+      <Input
+        placeholder="Basic usage"
+        onChange={(e) => setproductName(e.target.value)}
+      />
 
       <br />
       <h5> product Description</h5>
       <br />
       <div className="wyswing">
         <Editor
+          onChange={(e) => setproductDescription(e.blocks[0].text)}
           editorState={editorState}
           toolbarClassName="toolbarClassName"
           wrapperClassName="wrapperClassName"
@@ -102,13 +174,37 @@ const AddProduct = () => {
 
       <h5>Select Store</h5>
       <Select
+        onChange={(e) => {
+          setStoreName(e);
+        }}
         mode="multiple"
         tagRender={tagRender}
         style={{
           width: "100%",
+          color: "red",
         }}
         options={AllstoreData || options}
       />
+      <br />
+      <br />
+      <br />
+      <br />
+      <PrdouctUpload />
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <Button
+        style={{ width: "100%" }}
+        type="primary"
+        shape="round"
+        icon={<UploadOutlined />}
+        size={size}
+        onClick={HandleProductUpload}
+      >
+        Download
+      </Button>
     </>
   );
 };
---
+++ src/Component/ProductUplad/PrdouctUpload.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, message, Upload } from "antd";
+const props = {
+  name: "file",
+  action: "https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188",
+  headers: {
+    authorization: "authorization-text",
+  },
+  onChange(info) {
+    if (info.file.status !== "uploading") {
+      console.log(info.file, info.fileList);
+    }
+    if (info.file.status === "done") {
+      message.success(`${info.file.name} file uploaded successfully`);
+    } else if (info.file.status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
+  },
+  progress: {
+    strokeColor: {
+      "0%": "#108ee9",
+      "100%": "#87d068",
+    },
+    strokeWidth: 3,
+    format: (percent) => percent && `${parseFloat(percent.toFixed(2))}%`,
+  },
+};
+const PrdouctUpload = () => (
+  <Upload {...props}>
+    <Button icon={<UploadOutlined />}> Upload Image</Button>
+  </Upload>
+);
+export default PrdouctUpload;
