669be2dfc98ea4f60b396a8bf5e7c31b378b03f3
Taufik islam <97851815+taufik69@users.noreply.github.com>
2024-01-29 22:02:53 +0600

    forgot password

--- src/Component/ForgotPassword/ForgotPassword.jsx
+++ src/Component/ForgotPassword/ForgotPassword.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import React from "react";
+import { Card, Input, Button } from "antd";
 const ForgotPassword = () => {
   return (
-    <div>ForgotPassword</div>
-  )
-}
+    <div>
+      <Card
+        title="Forgot Password"
+        bordered={false}
+        style={{
+          width: "400px",
+          margin: "30vh auto",
+        }}
+      >
+        <label htmlFor="email">Please give your Email</label>
+        <Input
+          placeholder="email"
+          id="email"
+          name="email"
+          style={{ marginTop: "6px" }}
+        />
+        <Button type="primary " block style={{ marginTop: "15px" }}>
+          send
+        </Button>
+      </Card>
+      
+    </div>
+  );
+};
 
-export default ForgotPassword
+export default ForgotPassword;
